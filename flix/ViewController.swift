6e330af51539936b608eca504750971b9f20414a
Quentin-Allen Velayo Fernandez <qfernandez@csumb.edu>
2020-02-04 12:33:20 -0800

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Quentin-Allen Velayo Fernandez on 2/4/20.
+//  Copyright © 2020 Quentin-Allen Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
