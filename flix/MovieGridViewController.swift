75a8faa85276d35f4607769b99dd12d9f49a3d0b
Quentin-Allen Velayo Fernandez <qfernandez@csumb.edu>
2020-02-09 23:27:31 -0800

    flix v2 update

---
+++ flix/MovieGridViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieGridViewController.swift
+//  flix
+//
+//  Created by Quentin-Allen Velayo Fernandez on 2/9/20.
+//  Copyright © 2020 Quentin-Allen Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
